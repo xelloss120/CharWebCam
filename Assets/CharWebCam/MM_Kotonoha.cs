d56bbcfa5938e200bd54ee9e3ebc8250c03bb0a7
xelloss120 <xelloss120@hotmail.com>
2017-07-10 01:30:24 +0900

    cs add test

---
+++ Assets/CharWebCam/MM_Kotonoha.cs
@@ -0,0 +1,13 @@
+﻿public class MM_Kotonoha : MouthMove
+{
+    void Start()
+    {
+        Init();
+    }
+
+    void Update()
+    {
+        float vol = GetVolume();
+        GetComponent<MMD4MecanimModel>().GetMorph("あ").weight = SmoothMouth.SmoothValue(vol < 1 ? 0 : vol / 20);
+    }
+}
