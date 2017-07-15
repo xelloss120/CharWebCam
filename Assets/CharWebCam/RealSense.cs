fc753f94e6ef64a14c9bf40d02455a665972000f
xelloss120 <xelloss120@hotmail.com>
2017-07-16 04:51:03 +0900

    add face exp & Refactoring

--- Assets/CharWebCam/RealSense.cs
+++ Assets/CharWebCam/RealSense.cs
@@ -10,19 +10,57 @@ public class RealSense : MonoBehaviour
 {
     // 例外表示
     public Text Text;
+    public GameObject Body;
+
+    // キャラクター制御パラメーター
+    protected Vector3 BodyPos;
+    protected Vector3 HeadAng;
+    protected Vector2 EyesPos;
+    protected float EyesClose;
+    protected float BrowRai;
+    protected float BrowLow;
+    protected float Smile;
+    protected float Kiss;
+    protected float Mouth;
+    protected float Tongue;
+
+    // キャラクター制御パラメーターの調整値
+    protected float BodyPosX = 3;
+    protected float BodyPosY = 3;
+    protected float BodyPosZ = 500;
+    protected float BodyPosYOffset;
+    protected int BodyPosSmoothWeight = 20;
+    protected float HeadAngX = 70;
+    protected float HeadAngY = 90;
+    protected float HeadAngZ = 300;
+    protected int HeadAngSmoothWeight = 20;
+    protected float EyesPosX = 0.8f;
+    protected float EyesPosY = 0.2f;
+    protected int EyesPosSmoothWeight = 5;
+    protected int EyesCloseSmoothWeight = 3;
+    protected int FaceSmoothWeight = 3;
+
+    // 検出値取得
+    protected bool Ready = false;
 
     // 検出値
-    protected ImageInfo Resolution;
-    protected RectI32 FaceRect;
-    protected LandmarkPoint[] Landmark;
-    protected Dictionary<FaceExpression, FaceExpressionResult> FaceExp;
+    ImageInfo Resolution;
+    RectI32 FaceRect;
+    LandmarkPoint[] Landmark;
+    Dictionary<FaceExpression, FaceExpressionResult> FaceExp;
 
     // 平滑化
-    protected Smoother3D SmoothBody;
-    protected Smoother3D SmoothHead;
-    protected Smoother2D SmoothEyes;
-    protected Smoother1D SmoothEyesClose;
     Smoother Smoother;
+    Smoother3D SmoothBody;
+    Smoother3D SmoothHead;
+    Smoother2D SmoothEyes;
+    Smoother1D SmoothEyesClose;
+    Smoother1D SmoothBrowRai;
+    Smoother1D SmoothBrowLow;
+    Smoother1D SmoothSmile;
+    Smoother1D SmoothKiss;
+    Smoother1D SmoothMouth;
+    Smoother1D SmoothTongue;
 
     // RealSense
     SenseManager SenseManager;
@@ -32,6 +70,9 @@ public class RealSense : MonoBehaviour
 
     protected void Init()
     {
+        // 初期表示位置(オフセット)の保持
+        BodyPosYOffset = Body.transform.position.y; ;
+
         try
         {
             // RealSense初期化
@@ -59,10 +100,16 @@ public class RealSense : MonoBehaviour
             // 参考：https://software.intel.com/sites/landingpage/realsense/camera-sdk/v2016r3/documentation/html/index.html?doc_utils_the_smoother_utility.html
             Smoother = Smoother.CreateInstance(SenseManager.Session);
 
-            SmoothBody = Smoother.Create3DWeighted(20);
-            SmoothHead = Smoother.Create3DWeighted(20);
-            SmoothEyes = Smoother.Create2DWeighted(5);
-            SmoothEyesClose = Smoother.Create1DWeighted(5);
+            SmoothBody = Smoother.Create3DWeighted(BodyPosSmoothWeight);
+            SmoothHead = Smoother.Create3DWeighted(HeadAngSmoothWeight);
+            SmoothEyes = Smoother.Create2DWeighted(EyesPosSmoothWeight);
+            SmoothEyesClose = Smoother.Create1DWeighted(EyesCloseSmoothWeight);
+            SmoothBrowRai = Smoother.Create1DWeighted(FaceSmoothWeight);
+            SmoothBrowLow = Smoother.Create1DWeighted(FaceSmoothWeight);
+            SmoothSmile = Smoother.Create1DWeighted(FaceSmoothWeight);
+            SmoothKiss = Smoother.Create1DWeighted(FaceSmoothWeight);
+            SmoothMouth = Smoother.Create1DWeighted(FaceSmoothWeight);
+            SmoothTongue = Smoother.Create1DWeighted(FaceSmoothWeight);
         }
         catch (Exception e)
         {
@@ -80,10 +127,139 @@ public class RealSense : MonoBehaviour
         var face = FaceData.QueryFaceByIndex(0);
         if (face != null)
         {
+            // 検出値
             FaceRect = face.Detection.BoundingRect;
             Landmark = face.Landmarks.Points;
             FaceExp = face.Expressions.ExpressionResults;
+
+            // 体位置
+            BodyPos = SmoothBody.SmoothValue(GetBodyPos(FaceRect));
+
+            // 頭角度
+            HeadAng = SmoothHead.SmoothValue(GetHeadAng(Landmark));
+
+            // 視線
+            EyesPos = SmoothEyes.SmoothValue(GetEyesPos(Landmark));
+
+            // 目パチ
+            float eyeL = FaceExp[FaceExpression.EXPRESSION_EYES_CLOSED_LEFT].intensity;
+            float eyeR = FaceExp[FaceExpression.EXPRESSION_EYES_CLOSED_RIGHT].intensity;
+            EyesClose = SmoothEyesClose.SmoothValue(Mathf.Max(eyeL, eyeR));
+
+            // 眉上
+            float browLowL = FaceExp[FaceExpression.EXPRESSION_BROW_LOWERER_LEFT].intensity;
+            float browLowR = FaceExp[FaceExpression.EXPRESSION_BROW_LOWERER_RIGHT].intensity;
+            BrowLow = SmoothBrowLow.SmoothValue(Mathf.Max(browLowL, browLowR));
+
+            // 眉下
+            float browRaiL = FaceExp[FaceExpression.EXPRESSION_BROW_RAISER_LEFT].intensity;
+            float browRaiR = FaceExp[FaceExpression.EXPRESSION_BROW_RAISER_RIGHT].intensity;
+            BrowRai = SmoothBrowRai.SmoothValue(Mathf.Max(browRaiL, browRaiR));
+
+            // 笑顔
+            Smile = SmoothSmile.SmoothValue(FaceExp[FaceExpression.EXPRESSION_SMILE].intensity);
+
+            // キス(口開と若干競合)
+            Kiss = SmoothKiss.SmoothValue(FaceExp[FaceExpression.EXPRESSION_KISS].intensity);
+
+            // 口開(キスと若干競合)
+            Mouth = SmoothMouth.SmoothValue(FaceExp[FaceExpression.EXPRESSION_MOUTH_OPEN].intensity);
+
+            // べー(口開と競合)
+            Tongue = SmoothTongue.SmoothValue(FaceExp[FaceExpression.EXPRESSION_TONGUE_OUT].intensity);
+
+            Ready = true;
         }
+    }
+
+    /// <summary>
+    /// 体位置を取得
+    /// </summary>
+    /// <param name="rect">顔の矩形</param>
+    /// <returns>体位置</returns>
+    /// <remarks>
+    /// https://software.intel.com/sites/landingpage/realsense/camera-sdk/v2016r3/documentation/html/index.html?doc_face_face_location_data.html
+    /// </remarks>
+    Vector3 GetBodyPos(RectI32 rect)
+    {
+        // 体位置に利用するため頭位置を取得
+        float xMax = Resolution.width;
+        float yMax = Resolution.height;
+        float xPos = rect.x + (rect.w / 2);
+        float yPos = rect.h + (rect.h / 2);
+        float zPos = (yMax - rect.h);
+
+        // 末尾の除算で調整
+        xPos = (xPos - (xMax / 2)) / (xMax / 2) / BodyPosX;
+        yPos = (yPos - (yMax / 2)) / (yMax / 2) / BodyPosY;
+        zPos = zPos / BodyPosZ;
+
+        // 初期位置のオフセットを適用
+        yPos += BodyPosYOffset;
+
+        // 顔の大きさと中心から初期位置分ずらして体位置に利用
+        return new Vector3(-xPos, yPos, zPos);
+    }
+
+    /// <summary>
+    /// 頭角度を取得
+    /// </summary>
+    /// <param name="points">顔の検出点</param>
+    /// <returns>頭角度</returns>
+    /// <remarks>
+    /// https://software.intel.com/sites/landingpage/realsense/camera-sdk/v2016r3/documentation/html/index.html?doc_face_face_landmark_data.html
+    /// </remarks>
+    Vector3 GetHeadAng(LandmarkPoint[] points)
+    {
+        // 頭向きに利用するため顔の中心と左右端、唇下、顎下を取得
+        Vector2 center = points[29].image;
+        Vector2 left = points[68].image;
+        Vector2 right = points[54].image;
+        Vector2 mouth = points[42].image;
+        Vector2 chin = points[61].image;
+
+        // 末尾で調整(0.2は顔幅に対する唇下から顎までの比 / 300はその値に対する倍率 / 10.416はUnityちゃん初期値)
+        float xAng = (Vector2.Distance(left, center) - Vector2.Distance(right, center)) / Vector2.Distance(left, right) * HeadAngX;
+        float yAng = GetAngle(mouth, chin) - HeadAngY;
+        float zAng = (Vector2.Distance(mouth, chin) / Vector2.Distance(left, right) - 0.2f) * HeadAngZ;
+
+        // 唇下と顎下の点から角度計算して頭向きに利用
+        return new Vector3(xAng, -yAng, zAng);
+    }
+
+    /// <summary>
+    /// 瞳位置を取得
+    /// </summary>
+    /// <param name="points">顔の検出点</param>
+    /// <returns>瞳位置</returns>
+    /// <remarks>
+    /// https://software.intel.com/sites/landingpage/realsense/camera-sdk/v2016r3/documentation/html/index.html?doc_face_face_landmark_data.html
+    /// </remarks>
+    Vector2 GetEyesPos(LandmarkPoint[] points)
+    {
+        // 左右の目の瞳と上下左右端を取得
+        Vector2 lEye = points[77].image;
+        Vector2 lLeft = points[22].image;
+        Vector2 lRight = points[18].image;
+        Vector2 lTop = points[20].image;
+        Vector2 lBottom = points[24].image;
+        Vector2 rEye = points[76].image;
+        Vector2 rLeft = points[10].image;
+        Vector2 rRight = points[14].image;
+        Vector2 rTop = points[12].image;
+        Vector2 rBottom = points[16].image;
+
+        // 末尾で調整
+        float tmp1, tmp2;
+        tmp1 = GetCenterRatio(lRight, lEye, lLeft) * EyesPosX;
+        tmp2 = GetCenterRatio(rRight, rEye, rLeft) * EyesPosX;
+        float xPos = (tmp1 + tmp2) / 2;
+        tmp1 = GetCenterRatio(lTop, lEye, lBottom) * EyesPosY;
+        tmp2 = GetCenterRatio(rTop, rEye, rBottom) * EyesPosY;
+        float yPos = (tmp1 + tmp2) / 2;
+
+        // 唇下と顎下の点から角度計算して頭向きに利用
+        return new Vector2(xPos, yPos);
     }
 
     /// <summary>
@@ -117,6 +293,12 @@ public class RealSense : MonoBehaviour
     void OnDestroy()
     {
         // 平滑化開放
+        SmoothTongue.Dispose();
+        SmoothMouth.Dispose();
+        SmoothKiss.Dispose();
+        SmoothSmile.Dispose();
+        SmoothBrowLow.Dispose();
+        SmoothBrowRai.Dispose();
         SmoothEyesClose.Dispose();
         SmoothEyes.Dispose();
         SmoothHead.Dispose();
--- MMD_Sample/RS_Kotonoha.cs
+++ MMD_Sample/RS_Kotonoha.cs
@@ -1,162 +1,77 @@
 ﻿using UnityEngine;
-using Intel.RealSense;
-using Intel.RealSense.Face;
 
 public class RS_Kotonoha : RealSense
 {
-    // オブジェクト
-    public GameObject Body;
     public GameObject Head;
     public GameObject EyeL;
     public GameObject EyeR;
-
-    // 初期表示位置(オフセット)
-    float BodyY;
     
     // モーフ取得のため
     MMD4MecanimModel Model;
 
     void Start()
     {
-        // RealSense初期化
-        Init();
+        EyesPosX = 20;
+        EyesPosY = 7;
 
-        // 初期表示位置(オフセット)の保持
-        BodyY = Body.transform.position.y;
+        Init();
 
         // 初期表情
         Model = GetComponent<MMD4MecanimModel>();
-        Model.GetMorph("じと目").weight = 0.5f;
+        Model.GetMorph("じと目").weight = 0.7f;
         Model.GetMorph("眼球縮小").weight = 0.2f;
     }
 
     void Update()
     {
         // 値が揃ってから参照
-        if (Landmark == null && FaceExp == null)
+        if (!Ready)
         {
             return;
         }
 
         // 体移動
-        Body.transform.position = SmoothBody.SmoothValue(GetBodyPos(FaceRect));
+        Body.transform.position = BodyPos;
 
         // 頭向き
-        Head.transform.localEulerAngles = SmoothHead.SmoothValue(GetHeadAng(Landmark));
+        Head.transform.localEulerAngles = new Vector3(-HeadAng.z, -HeadAng.x, HeadAng.y);
 
         // 視線
-        Vector2 eyesPos = SmoothEyes.SmoothValue(GetEyesPos(Landmark));
-        Vector3 eyesAng = new Vector3(eyesPos.x, eyesPos.y, 0);
+        Vector3 eyesAng = new Vector3(EyesPos.y, EyesPos.x, 0);
         EyeL.GetComponent<MMD4MecanimBone>().userEulerAngles = eyesAng;
         EyeR.GetComponent<MMD4MecanimBone>().userEulerAngles = eyesAng;
 
         // 目パチ
-        float eyeL = FaceExp[FaceExpression.EXPRESSION_EYES_CLOSED_LEFT].intensity;
-        float eyeR = FaceExp[FaceExpression.EXPRESSION_EYES_CLOSED_RIGHT].intensity;
-        float close = SmoothEyesClose.SmoothValue(Mathf.Max(eyeL, eyeR)) / 100;
-        Model.GetMorph("まばたき").weight = close;
-        Model.GetMorph("じと目").weight = 0.5f - (close / 2);
+        Model.GetMorph("まばたき").weight = EyesClose / 100;
+        Model.GetMorph("じと目").weight = 0.7f - (EyesClose * 0.007f);
+
+        // 眉上
+        Model.GetMorph("上").weight = BrowRai / 100;
+
+        // 眉下
+        Model.GetMorph("下").weight = BrowLow / 100;
+        Model.GetMorph("困る").weight = BrowLow / 100;
+        Model.GetMorph("上まぶた閉").weight = BrowLow / 100;
 
         // 笑顔
-        float smile = FaceExp[FaceExpression.EXPRESSION_SMILE].intensity / 100;
-        Model.GetMorph("まばたき3").weight = smile;
-        Model.GetMorph("笑い").weight = smile;
+        Model.GetMorph("まばたき3").weight = Smile / 100;
+        Model.GetMorph("笑い").weight = Smile / 100;
 
-        // 表情優先の目パチ無効
-        if (smile != 0)
+        // キス
+        Model.GetMorph("眼球縮小").weight = Kiss * 0.02f + 0.2f;
+
+        // 表情競合対策
+        if (Smile != 0 || BrowLow != 0)
         {
             Model.GetMorph("まばたき").weight = 0;
-            Model.GetMorph("じと目").weight = 0.5f - (smile / 2);
+
+            float ret = Mathf.Max(Smile, BrowLow);
+            Model.GetMorph("じと目").weight = 0.7f - (ret * 0.007f);
         }
-    }
-
-    /// <summary>
-    /// 体位置を取得
-    /// </summary>
-    /// <param name="rect">顔の矩形</param>
-    /// <returns>体位置</returns>
-    /// <remarks>
-    /// https://software.intel.com/sites/landingpage/realsense/camera-sdk/v2016r3/documentation/html/index.html?doc_face_face_location_data.html
-    /// </remarks>
-    Vector3 GetBodyPos(RectI32 rect)
-    {
-        // 体位置に利用するため頭位置を取得
-        float xMax = Resolution.width;
-        float yMax = Resolution.height;
-        float xPos = rect.x + (rect.w / 2);
-        float yPos = rect.h + (rect.h / 2);
-        float zPos = (yMax - rect.h);
-
-        // 末尾の除算で調整
-        xPos = (xPos - (xMax / 2)) / (xMax / 2) / 3;
-        yPos = (yPos - (yMax / 2)) / (yMax / 2) / 3;
-        zPos = zPos / 500;
-
-        // 初期位置のオフセットを適用
-        yPos += BodyY;
-
-        // 顔の大きさと中心から初期位置分ずらして体位置に利用
-        return new Vector3(-xPos, yPos, zPos);
-    }
-
-    /// <summary>
-    /// 頭角度を取得
-    /// </summary>
-    /// <param name="points">顔の検出点</param>
-    /// <returns>頭角度</returns>
-    /// <remarks>
-    /// https://software.intel.com/sites/landingpage/realsense/camera-sdk/v2016r3/documentation/html/index.html?doc_face_face_landmark_data.html
-    /// </remarks>
-    Vector3 GetHeadAng(LandmarkPoint[] points)
-    {
-        // 頭向きに利用するため顔の中心と左右端、唇下、顎下を取得
-        Vector2 center = points[29].image;
-        Vector2 left = points[68].image;
-        Vector2 right = points[54].image;
-        Vector2 mouth = points[42].image;
-        Vector2 chin = points[61].image;
-
-        // 末尾で調整(0.2は顔幅に対する唇下から顎までの比 / 300はその値に対する倍率)
-        float xAng = (Vector2.Distance(mouth, chin) / Vector2.Distance(left, right) - 0.2f) * 300;
-        float yAng = (Vector2.Distance(left, center) - Vector2.Distance(right, center)) / Vector2.Distance(left, right) * 70;
-        float zAng = GetAngle(mouth, chin) - 90;
-
-        // 唇下と顎下の点から角度計算して頭向きに利用
-        return new Vector3(-xAng, -yAng, -zAng);
-    }
-
-    /// <summary>
-    /// 瞳位置を取得
-    /// </summary>
-    /// <param name="points">顔の検出点</param>
-    /// <returns>瞳位置</returns>
-    /// <remarks>
-    /// https://software.intel.com/sites/landingpage/realsense/camera-sdk/v2016r3/documentation/html/index.html?doc_face_face_landmark_data.html
-    /// </remarks>
-    Vector2 GetEyesPos(LandmarkPoint[] points)
-    {
-        // 左右の目の瞳と上下左右端を取得
-        Vector2 lEye = points[77].image;
-        Vector2 lLeft = points[22].image;
-        Vector2 lRight = points[18].image;
-        Vector2 lTop = points[20].image;
-        Vector2 lBottom = points[24].image;
-        Vector2 rEye = points[76].image;
-        Vector2 rLeft = points[10].image;
-        Vector2 rRight = points[14].image;
-        Vector2 rTop = points[12].image;
-        Vector2 rBottom = points[16].image;
-
-        // 末尾で調整
-        float tmp1, tmp2;
-        tmp1 = GetCenterRatio(lTop, lEye, lBottom) * 7;
-        tmp2 = GetCenterRatio(rTop, rEye, rBottom) * 7;
-        float xPos = (tmp1 + tmp2) / 2;
-        tmp1 = GetCenterRatio(lRight, lEye, lLeft) * 20;
-        tmp2 = GetCenterRatio(rRight, rEye, rLeft) * 20;
-        float yPos = (tmp1 + tmp2) / 2;
-
-        // 唇下と顎下の点から角度計算して頭向きに利用
-        return new Vector2(xPos, yPos);
+        if(Smile > BrowLow / 2)
+        {
+            Model.GetMorph("下").weight = 0;
+            Model.GetMorph("上まぶた閉").weight = 0;
+        }
     }
 }
