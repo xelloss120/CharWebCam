3d6d90194eef0f6b69bf3048fc2a69bfc6935153
xelloss120 <xelloss120@hotmail.com>
2017-07-17 20:10:59 +0900

    RawStream Hide

---
+++ Assets/CharWebCam/Hide.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Hide : MonoBehaviour
+{
+    public GameObject Target;
+
+    void Start()
+    {
+        StartCoroutine("GetKeyEsc");
+    }
+
+    IEnumerator GetKeyEsc()
+    {
+        while (!Input.GetKey(KeyCode.Escape)) yield return null;
+        Target.SetActive(false);
+    }
+}
