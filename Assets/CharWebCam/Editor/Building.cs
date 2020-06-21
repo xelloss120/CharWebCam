a9ea9ed0363efa32a0d1566f05e65b4ceb89888b
100の人 <100@pokemori.jp>
2020-06-21 21:00:15 +0900

    ビルド先の不要なファイルを削除するようにした

--- Assets/CharWebCam/Editor/Building.cs
+++ Assets/CharWebCam/Editor/Building.cs
@@ -1,6 +1,8 @@
+using System.Linq;
 using System.IO;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.Callbacks;
 
 internal class Building
 {
@@ -8,5 +10,39 @@ internal class Building
     static void SetRuntimePath()
     {
         RSPostBuildProcessor.SpecifiedRuntimePath = Path.Combine(Application.dataPath, "RSSDK");
+    }
+
+    [PostProcessBuild(2)]
+    static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+    {
+        var directoryPath = Path.GetDirectoryName(pathToBuiltProject);
+        RemoveUnnecessaryFiles(directoryPath);
+    }
+
+    /// <summary>
+    /// ビルド先の不要なファイルを削除
+    /// </summary>
+    static void RemoveUnnecessaryFiles(string directoryPath)
+    {
+        File.Delete(Path.Combine(directoryPath, "UnityCrashHandler64.exe"));
+
+        foreach (var group in Directory.GetFiles(directoryPath, "*.dll", SearchOption.AllDirectories)
+            .GroupBy(path => Path.GetFileName(path)))
+        {
+            if (group.Count() == 1)
+            {
+                continue;
+            }
+
+            var (shortPath, longPath) = group.ElementAt(0).Length < group.ElementAt(1).Length
+                ? (group.ElementAt(0), group.ElementAt(1))
+                : (group.ElementAt(1), group.ElementAt(0));
+            File.Delete(group.Key.EndsWith("_c.dll") ? longPath : shortPath);
+        }
+
+        foreach (var path in Directory.GetFiles(directoryPath, "*.meta", SearchOption.AllDirectories))
+        {
+            File.Delete(path);
+        }
     }
 }
