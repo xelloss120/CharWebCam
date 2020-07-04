24443b2ea5053445f2899ff4aa1ade1e0c6c4ff5
100の人 <100@pokemori.jp>
2020-07-04 09:20:36 +0900

    起動時にVRMファイル選択ダイアログを表示するようにした (コマンドライン引数「--vrm」が指定されていない場合)

--- Assets/CharWebCam/UI.cs
+++ Assets/CharWebCam/UI.cs
@@ -5,12 +5,14 @@ using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 using VRM;
+using VRMViewer;
 
 public class UI : MonoBehaviour
 {
     public Text Text;
     public Text DetectedValue;
     public RawImage RawImage;
+    public VRMPreviewUI ModalWindowPrefab;
 
     /// <summary>
     /// 同梱するVRMファイルの絶対パスを取得する
@@ -23,56 +25,95 @@ public class UI : MonoBehaviour
 
     async void Start()
     {
-        GameObject avatar = null;
-        if (!string.IsNullOrWhiteSpace(CommandLineArgs.VRM))
+        string path = null;
+        VRMImporterContext context = null;
+        try
         {
-            try
+            if (!string.IsNullOrWhiteSpace(CommandLineArgs.VRM))
             {
-                avatar = await Load(CommandLineArgs.VRM);
+                context = await Load(CommandLineArgs.VRM);
             }
-            catch (Exception exception)
+            else
             {
-                Text.text += "Failed to load the specified VRM file.\n"
-                    + $"Path: {CommandLineArgs.VRM}\n"
-                    + $"Error message:\n{exception.Message}\n"
-                    + "\n";
+                path = AskVRMPath();
+                if (path != null)
+                {
+                    context = await Load(path);
+                }
             }
         }
-
-        if (avatar == null)
+        catch (Exception exception)
         {
-            avatar = await Load(GetDefaultModelPath());
+            context = null;
+            DisplayLoadingError(path, exception);
         }
 
-        avatar.AddComponent<RS_VRM>().UI = this;
+        if (context == null)
+        {
+            context = await Load(GetDefaultModelPath());
+        }
+
+        Setup(context);
+
         StartCoroutine("GetKeyEsc");
     }
 
     /// <summary>
-    /// VRMファイルを読み込み、シーンへ配置する
+    /// VRMファイルを読み込む
     /// </summary>
-    /// <param name="path">VRMファイルの絶対パス</param>
+    /// <param name="bytes"></param>
     /// <returns></returns>
-    async Task<GameObject> Load(string path)
+    async Task<VRMImporterContext> Load(string path)
     {
         var context = new VRMImporterContext();
         try
         {
             context.ParseGlb(File.ReadAllBytes(path));
             await context.LoadAsyncTask();
-
-            var avatar = context.Root;
-            avatar.transform.parent = null;
-
-            context.ShowMeshes();
-
-            return avatar;
+            return context;
         }
         catch
         {
             context.Dispose();
             throw;
         }
+    }
+
+    /// <summary>
+    /// VRMファイル選択ダイアログを開く
+    /// </summary>
+    /// <returns></returns>
+    string AskVRMPath()
+    {
+        var path = FileDialogForWindows.FileDialog("使用するアバターを選択してください", ".vrm");
+        return string.IsNullOrEmpty(path) ? null : path;
+    }
+
+    /// <summary>
+    /// 指定したパスからのVRMファイルの読み込みに失敗したことを表示
+    /// </summary>
+    /// <param name="path"></param>
+    /// <param name="exception"></param>
+    void DisplayLoadingError(string path, Exception exception)
+    {
+        Debug.LogError(exception);
+
+        Text.text += "Failed to load the specified VRM file.\n"
+            + $"Path: {path}\n"
+            + $"Error message:\n{exception.Message}\n"
+            + "\n";
+    }
+
+    /// <summary>
+    /// VRMプレハブをシーン配置し、セットアップする
+    /// </summary>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    void Setup(VRMImporterContext context)
+    {
+        context.Root.transform.parent = null;
+        context.Root.AddComponent<RS_VRM>().UI = this;
+        context.ShowMeshes();
     }
 
     /// <summary>
